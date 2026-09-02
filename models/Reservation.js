c5676521d87b60d02a8582c90032bbf37e06e607
Chris <102741531+ratherpixelate@users.noreply.github.com>
2026-09-02 15:59:35 +0000

    feat: add Reservation schema

---
+++ models/Reservation.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const reservationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  time: { type: String, required: true },
+  guests: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Reservation', reservationSchema);
